02eca73c83928228022ac47fa8c28c33456e91eb
Luke Yeager <luke.yeager@gmail.com>
2016-06-24 21:29:29 +0000

    Add scripts for building from a binary

---
+++ from-binary/make_cuda-license-7-5.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+wget -nc http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-license-7-5_7.5-18_amd64.deb
+export OLD_PACKAGE=cuda-license-7-5_7.5-18_amd64.deb
+export NEW_VERSION=7.5-18ppa1
+
+make
