8be010ab39b7dcb0f316c32768551aaee4bf9a0f
Luke Yeager <luke.yeager@gmail.com>
2016-06-29 16:01:11 -0700

    Add similar cosmetic changes to from-binary

--- from-binary/make_cuda-license-7-5.sh
+++ from-binary/make_cuda-license-7-5.sh
@@ -5,4 +5,4 @@ wget -nc http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_
 export OLD_PACKAGE=cuda-license-7-5_7.5-18_amd64.deb
 export NEW_VERSION=7.5-18ppa4
 
-make
+make build
