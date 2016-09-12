978f731012ddccdbae8de9cb2fa8f6a2898f3439
Luke Yeager <lukeyeager@users.noreply.github.com>
2016-09-12 09:18:26 -0700

    Flask-SocketIO v2.6

--- from-source/make_flask-socketio.sh
+++ from-source/make_flask-socketio.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
-export SOURCE_TARBALL=https://github.com/lukeyeager/Flask-SocketIO/archive/v0.6.0.tar.gz
-export DEBIAN_TARBALL=https://github.com/lukeyeager/Flask-SocketIO/archive/debian/0.6.0.tar.gz
+export SOURCE_TARBALL=https://github.com/lukeyeager/Flask-SocketIO/archive/v2.6.tar.gz
+export DEBIAN_TARBALL=https://github.com/lukeyeager/Flask-SocketIO/archive/debian/2.6.tar.gz
+export PPA_NAME=luke-yeager/digits
 
 cd `dirname $0`
 make build
