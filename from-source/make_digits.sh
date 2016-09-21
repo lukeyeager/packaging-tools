553cec33cc5010c8812600d61d61249d9eba3aab
Luke Yeager <lukeyeager@users.noreply.github.com>
2016-09-21 15:31:29 -0700

    Testing digits+setup packaging

--- from-source/make_digits.sh
+++ from-source/make_digits.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-export SOURCE_TARBALL=https://github.com/lukeyeager/DIGITS/archive/v3.0.0.tar.gz
-export DEBIAN_TARBALL=https://github.com/lukeyeager/DIGITS/archive/debian/3.0.0.tar.gz
+export SOURCE_TARBALL=https://github.com/lukeyeager/DIGITS/archive/setup.tar.gz
+export DEBIAN_TARBALL=https://github.com/lukeyeager/DIGITS/archive/debian/master.tar.gz
 
 cd `dirname $0`
 make build
