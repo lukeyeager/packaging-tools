4d68e6f30dde7ba53703c3112695c406f53f9a21
Luke Yeager <lukeyeager@users.noreply.github.com>
2016-12-05 16:44:49 -0800

    [TravisCI] Test all scripts
    Remove old DIGITS script

--- from-source/make_digits.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-export SOURCE_TARBALL=https://github.com/lukeyeager/DIGITS/archive/setup.tar.gz
-export DEBIAN_TARBALL=https://github.com/lukeyeager/DIGITS/archive/debian/master.tar.gz
-
-cd `dirname $0`
-make build
